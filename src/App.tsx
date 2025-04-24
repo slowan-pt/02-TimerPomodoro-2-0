69b03c1ab408e211e0754359c89d859778cc14f0
Sloan Nascimento <sloan.nascimento@gmail.com>
2025-04-24 10:02:55 -0300

    styled-components-2

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import {Button} from "./Components/Button";
+
+export function App() {
+
+  return (
+    <>
+    <Button variant="primary" />
+    <Button variant="secondary"/>
+    <Button variant="danger"/>
+    <Button variant="success"/>
+    <Button />
+    </>
+  )
+}
+
